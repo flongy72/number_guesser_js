612158d828dde80aee8603eb7c05bc1711cd4451
flongy72 <45362924+flongy72@users.noreply.github.com>
2022-06-22 12:38:48 +0100

    final

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget () {
+    return Math.floor(Math.random() * 10)
+}
+
+function compareGuesses (human, computer, target) {
+    const human_diff = Math.abs(target-human);
+    const computer_diff = Math.abs(target-computer);
+    return human_diff <= computer_diff;
+}
+
+function updateScore (winner) {
+    if (winner === "human") {
+        humanScore += 1
+    } else if (winner === "computer") {
+        computerScore += 1
+    }
+}
+
+function advanceRound () {
+    currentRoundNumber += 1
+}
 
